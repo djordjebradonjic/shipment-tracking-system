453e320c789b670e2c479d47408420740b5d4f01
djordjebradonjic <djordjebradonjic99@gmail.com>
2026-06-20 11:56:36 +0200

    Initial commit. Spring boot skeleton created with  Spring Initializr

---
+++ src/main/java/com/example/shipment_tracking_system/ShipmentTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.shipment_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shipment_tracking_system/ShipmentTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shipment_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
